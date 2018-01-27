fa5ce97cb7fb65d0cb6391181925ad00894c3d83
Mathew Polzin <matt.polzin@gmail.com>
2018-01-27 13:38:12 -0800

    Add some documentation and make all viewables start offscreen to the right

--- FullscreenViewerDemo/ViewerGroupController/ViewerGroupController.swift
+++ FullscreenViewerDemo/ViewerGroupController/ViewerGroupController.swift
@@ -8,6 +8,19 @@
 
 import UIKit
 
+/// A container for the entire group of viewables. Types of this protocol are responsible
+/// for the appearance of the container where the current viewable is displayed. In
+/// practice, ViewerGroupController also requires its ViewerGroupContainer to be
+/// a UIView itself and to have its viewable container set up within its bounds.
+///
+/// The goal is to leave the layout within its superview and the appearance of the
+/// "viewing window" (viewableContainer) to the coder while the
+/// ViewerGroupController displays one viewable at a time within the viewableContainer.
+///
+/// |-------------| <- Viewer group container (UIVIew).
+/// |    |==|     | <- viewable container (UIView) constrained within groupe container.
+/// |-------------|
+///
 public protocol ViewerGroupContainer {
 	var viewableContainer: UIView { get }
 }
@@ -67,7 +80,11 @@ public class ViewerGroupController<ContainerViewType: UIView>: UIViewController,
 	func commonInit() {
 		view.clipsToBounds = true
 		
-		viewableGroup.forEach { $0.delegate = self }
+		let offscreenRight = CGRect(x: UIScreen.main.bounds.width, y: 0, width: 10, height: 10)
+		viewableGroup.forEach { viewable in
+			viewable.delegate = self
+			viewable.view.frame = offscreenRight
+		}
 	}
 	
 	public override func viewDidLoad() {
